0c624ccf5dd858f1b54aed9beed8a2962ac928d8
Thiot Thierry-Philippe <tthiot@SA203-07A.ensi-bourges.fr>
2017-04-12 14:04:21 +0200

    OpenCSV

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -1,7 +1,11 @@
 
 package example;
 
+import com.opencsv.*;
+import java.io.*;
 import org.apache.commons.lang.WordUtils;
+import java.util.*;
+import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
@@ -9,11 +13,23 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
-    	int monmax=0;
-    	CSVReader reader=new CSVReader(new FileReader("data.csv"));
-    	List<String[]> myEntries = reader.readAll();
-    	//parcourt
-    	//System.out.println("Nombre lu:" + nb)
+    	try{
+          int monmax=0;
+          CSVReader csvReader = new CSVReader(new FileReader("data.csv"));
+          //System.out.println("test 1");
+          List content = csvReader.readAll();
+          //System.out.println("test 2");
+          String[] row;
+          for (Object object : content) {
+            //System.out.println("test "+monmax);
+              row = (String[]) object;
+              //System.out.println(row[0] + " # " + row[1] + " #  " + row[2]);
+              monmax++;
+          }
+          csvReader.close();
+          System.out.println("Nombre lu : "+monmax);
+        }
+        catch(FileNotFoundException e){}
+        catch(IOException e){}
     }
 }
-        
